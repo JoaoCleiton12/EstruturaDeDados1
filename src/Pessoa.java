4e7bd6c6b071e98e639c08b50b62eabd9922ea33
JoaoCleiton <joaocleiton60359@gmail.com>
2023-02-24 17:23:01 -0300

    questao31 feita

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,55 +1,47 @@
-public class Pessoa {
+public class pessoa {
+        private long cpf;
+        private String nome;
+        private int idade;
+        private String sexo;
+        private double peso;
+        private double altura;
+        private double imc;
     
-    private int cpf;
-    private String nome;
-    private int idade;
-    private String sexo;
-    private float peso;
-    private float altura;
-    private double imc;
+        public pessoa(long cpf, String nome, int idade, String sexo, double peso, double altura) {
+            this.cpf = cpf;
+            this.nome = nome;
+            this.idade = idade;
+            this.sexo = sexo;
+            this.peso = peso;
+            this.altura = altura;
+            this.imc = (peso / (altura * altura));
+        }
     
-    public int getCpf() {
-        return cpf;
-    }
-    public void setCpf(int cpf) {
-        this.cpf = cpf;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public int getIdade() {
-        return idade;
-    }
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-    public String getSexo() {
-        return sexo;
-    }
-    public void setSexo(String sexo) {
-        this.sexo = sexo;
-    }
-    public float getPeso() {
-        return peso;
-    }
-    public void setPeso(float peso) {
-        this.peso = peso;
-    }
-    public float getAltura() {
-        return altura;
-    }
-    public void setAltura(float altura) {
-        this.altura = altura;
-    }
-    public double getImc() {
-        return imc;
-    }
-    public void setImc(double imc) {
-        this.imc = imc;
-    }
-
+        public long getCpf() {
+            return cpf;
+        }
     
-}
+        public String getNome() {
+            return nome;
+        }
+    
+        public int getIdade() {
+            return idade;
+        }
+    
+        public String getSexo() {
+            return sexo;
+        }
+    
+        public double getPeso() {
+            return peso;
+        }
+    
+        public double getAltura() {
+            return altura;
+        }
+    
+        public double calcularIMC() {
+            return imc;
+        }
+     }
--- src/Questao31.java
+++ src/Questao31.java
@@ -4,46 +4,51 @@
     de execução. Ao cadastrar uma pessoa, calcule o IMC. Ao final, exiba a lista de pessoas(com seus atributos).
  */
 
- import java.util.Scanner;
+ import java.util.ArrayList;
+import java.util.Scanner;
 
  public class Questao31 {
  
-     private static Scanner scanner = new Scanner(System.in);
- 
-     public static void main(String args[]){
-         System.out.print("Quantas pessoas serão cadastradas: ");
-         int tam = scanner.nextInt();
-        
-         Pessoa individuo[] = new Pessoa[1];
-         System.out.println("Cadastrando uma nova pessoa:");
-             System.out.print("Nome: ");
-             String nome = scanner.nextLine();
-             individuo[0].setNome(nome);
-         /* for(int i = 0; i<tam;i++){
-             System.out.println("Cadastrando uma nova pessoa:");
-             System.out.print("Nome: ");
-             String nome = scanner.nextLine();
-             individuo[i].setNome(nome);
-             System.out.print("idade: ");
-             int idade = scanner.nextInt();
-             individuo[i].setIdade(idade);
-             System.out.print("cpf: ");
-             int cpf = scanner.nextInt();
-             individuo[i].setCpf(cpf);
-             System.out.print("sexo: ");
-             String sexo = scanner.nextLine();
-             individuo[i].setSexo(sexo);
-             System.out.print("peso: ");
-             float peso = scanner.nextFloat();
-             individuo[i].setPeso(peso);
-             System.out.print("altura: ");
-             float altura = scanner.nextFloat();
-             individuo[i].setAltura(altura);
-         } */
- 
-         System.out.print(individuo[0].getNome());
- 
-     }
-     
- }
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        //criando lista de pessoas
+        ArrayList<pessoa> individuos = new ArrayList<pessoa>();
+
+        System.out.print("Quantas pessoas serão cadastradas? ");
+        int tam = scanner.nextInt();
+        System.out.println();
+        for (int i = 0; i < tam; i++) {
+            System.out.print("Cadastrando pessoa "+ (i+1));
+            System.out.print("\nCPF: ");
+            long cpf = scanner.nextLong();
+            scanner.nextLine(); // Consumir a quebra de linha
+            System.out.print("Nome: ");
+            String nome = scanner.nextLine();
+            System.out.print("Idade: " );
+            int idade = scanner.nextInt();
+            scanner.nextLine(); // Consumir a quebra de linha
+            System.out.print("Sexo: " );
+            String sexo = scanner.nextLine();
+            System.out.print("Peso: " );
+            double peso = scanner.nextDouble();
+            scanner.nextLine();
+            System.out.print("Altura: ");
+            double altura = scanner.nextDouble();
+            pessoa temp = new pessoa(cpf, nome, idade, sexo, peso, altura);
+            individuos.add(temp);
+        }
+
+        System.out.println();
+        System.out.println("Pessoas cadastradas:");
+        System.out.println("CPF\t\tNome\tIdade\tSexo\tPeso (kg)\tAltura (m)\tIMC");
+        for (pessoa temp : individuos) {
+            System.out.println(temp.getCpf() + "\t" + temp.getNome() + "\t" + temp.getIdade() + "\t" + temp.getSexo() +
+                               "\t" + temp.getPeso() + "\t\t" + temp.getAltura() + "\t\t" + temp.calcularIMC());
+        }
+    }
+}
+
+
  
